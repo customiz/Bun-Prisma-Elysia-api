6bff448772d74f702a64ebc4b491ca41aba15315
Kokiat Tankoem <kokiat.t@tcc-technology,com>
2023-09-24 16:05:41 +0700

    create route

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,26 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+
+  .get("/", () => "Hello Elysia")
+  .get("/search",({query}) => `query: ${query.q}`)
+  .get("/search/movie",({query}) => `query: ${query.q}`)
+  .get("/search/tv",({query}) => `query: ${query.q}`)
+  .get("/search/person",({query}) => `query: ${query.q}`)
+  .get("/search/company",({query}) => `query: ${query.q}`)
+  .get("/search/episode",({query}) => `query: ${query.q}`)
+  .get("/search/review",({query}) => `query: ${query.q}`)
+  .get("/search/award",({query}) => `query: ${query.q}`)
+
+  .get("/title/:id",({params}) => `id: ${params.id}`)
+  .get("/title/:id/episodes",({params}) => `id: ${params.id}`)
+  .get("/title/:id/cast",({params}) => `id: ${params.id}`)
+  .get("/title/:id/reviews",({params}) => `id: ${params.id}`)
+  .get("/title/:id/awards",({params}) => `id: ${params.id}`)
+
+
+
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
