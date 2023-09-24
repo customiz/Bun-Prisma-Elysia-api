c51f993cb23062a904bd8e5c14426642e0edc29c
Kokiat Tankoem <kokiat.t@tcc-technology,com>
2023-09-24 16:29:09 +0700

    app guard

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,33 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
 
 const app = new Elysia()
-
   .get("/", () => "Hello Elysia")
-
   .group("/search", (app) => {
     return app
-      .get("/", ({ query }) => `query: ${query.q}`)
-      .get("/movie", ({ query }) => `query: ${query.q}`)
-      .get("/tv", ({ query }) => `query: ${query.q}`)
-      .get("/person", ({ query }) => `query: ${query.q}`)
-      .get("/company", ({ query }) => `query: ${query.q}`)
-      .get("/episode", ({ query }) => `query: ${query.q}`)
-      .get("/review", ({ query }) => `query: ${query.q}`)
-      .get("/award", ({ query }) => `query: ${query.q}`)
+      .guard({
+        query: t.Object({
+          q: t.String(),
+        }),
+      }, (app) =>
+        app
+          .get("/", ({ query }) => `query: ${query.q}`)
+          .get("/movie", ({ query }) => `query: ${query.q}`)
+          .get("/tv", ({ query }) => `query: ${query.q}`)
+          .get("/person", ({ query }) => `query: ${query.q}`)
+          .get("/company", ({ query }) => `query: ${query.q}`)
+          .get("/episode", ({ query }) => `query: ${query.q}`)
+          .get("/review", ({ query }) => `query: ${query.q}`)
+          .get("/award", ({ query }) => `query: ${query.q}`)
+      );
   })
-
   .group("/title/:id", (app) => {
     return app
       .get("/", ({ params }) => `id: ${params.id}`)
       .get("/episodes", ({ params }) => `id: ${params.id}`)
       .get("/cast", ({ params }) => `id: ${params.id}`)
       .get("/reviews", ({ params }) => `id: ${params.id}`)
-      .get("/awards", ({ params }) => `id: ${params.id}`)
+      .get("/awards", ({ params }) => `id: ${params.id}`);
   })
-
-
-
   .listen(3000);
 
 console.log(
